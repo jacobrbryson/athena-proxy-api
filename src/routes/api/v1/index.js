a7ca147834c7ee6806e424fe133ac1c3313c5be6
Ross Bryson <jacobrbryson@gmail.com>
2025-11-14 09:35:23 -0500

    CICD

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const proxy = require("../../../proxy/httpProxy");
+const { API_TARGET } = require("../../../config");
 
 const router = express.Router();
 
 router.use((req, res) => {
-	console.log(`Proxying ${req.method} ${req.url}`);
+	console.log(`Proxying ${req.method} ${API_TARGET}${req.url}`);
 	proxy.web(req, res);
 });
 
