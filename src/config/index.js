2f138934f6394bb208234301a174b963c5647515
Ross Bryson <jacobrbryson@gmail.com>
2026-03-23 10:01:54 -0400

    More ws updates

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	PORT: process.env.PORT || 8080,
 	API_TARGET: process.env.API_TARGET || "http://localhost:3001",
+	API_AUDIENCE: process.env.API_AUDIENCE || process.env.API_TARGET || "http://localhost:3001",
 	RATE_LIMIT_WINDOW_MS: process.env.RATE_LIMIT_WINDOW_MS || 60000,
 	RATE_LIMIT_MAX_REQUESTS: process.env.RATE_LIMIT_MAX_REQUESTS || 100,
 	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
